cf55ae5964c66c31a66e09442710194caf3ae792
karinafs-yo <karinafs@upi.edu>
2025-04-09 16:21:53 +0700

    Karina | Menambahkan struct dan array

--- index.cpp
+++ index.cpp
@@ -3,8 +3,21 @@ using namespace std;
 
 //? Struct
 struct antrian {
+    int no_antrian;
+    string nama_pasien;
+    string keluhan;
+    bool status;
+    string resep_obat[][];
+};
 
-}
+struct history {
+    antrian nama_pasien;
+    
+};
+
+
+
+
 
 
 
