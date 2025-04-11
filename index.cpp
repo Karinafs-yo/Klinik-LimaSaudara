8e4bbe526ecdb1085ef9f16a90c185f28c4d7070
rifdahm12 <rifdahm12@upi.edu>
2025-04-11 14:00:57 +0700

    Rifdah | Commit test

--- index.cpp
+++ index.cpp
@@ -3,26 +3,39 @@ using namespace std;
 
 //? Struct
 struct antrian {
-    int no_antrian;
+    string no_antrian;
     string nama_pasien;
     string keluhan;
     bool status;
     string resep_obat[][];
+    antrian *next, *prev;
 };
 
 struct history {
-    antrian nama_pasien;
-    
+    string no_antrian;
+    string nama_pasien;
+    string keluhan;
+    bool status;
+    string resep_obat[][];
+    history *next, *prev;
 };
 
-
-
-
-
-
+string obat[];
+double harga;
+int stok, jumlah_obat = 0;
+antrian *head_bpjs = NULL;
 
 //? Function
+void antrianBaru()
+{
+    string isBPJS;
+    cout << "BPJS = Ya / Tidak? "; cin >> isBPJS;
 
+    if (isBPJS == "Ya")
+    {
+        
+    }
+}
 
 
 
