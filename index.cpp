68302e6219e30938aa437d07a566c552e7565606
rifdahm12 <rifdahm12@upi.edu>
2025-04-11 15:26:33 +0700

    Rifdah | Membuat Antrian

--- index.cpp
+++ index.cpp
@@ -16,25 +16,53 @@ struct history {
     string nama_pasien;
     string keluhan;
     bool status;
-    string resep_obat[][];
+    string resep_obat[10][3];
     history *next, *prev;
 };
 
 string obat[];
 double harga;
-int stok, jumlah_obat = 0;
-antrian *head_bpjs = NULL;
+int stok, jumlah_obat = 0, no_antrian_BPJS = 0, no_antrian_general = 0;
+antrian *head_bpjs = NULL, *tail_bpjs = NULL, *head_general = NULL, *tail_general = NULL;
 
 //? Function
 void antrianBaru()
 {
     string isBPJS;
     cout << "BPJS = Ya / Tidak? "; cin >> isBPJS;
+    antrian *newAntrian = new antrian;
 
     if (isBPJS == "Ya")
     {
-        
+        newAntrian -> no_antrian = "B" + to_string(no_antrian_BPJS++);
+        if (head_bpjs == NULL)
+        {
+            newAntrian -> prev = tail_general;
+            head_bpjs = newAntrian;
+            tail_bpjs = newAntrian;
+        }
+        else
+        {
+            newAntrian -> prev = tail_bpjs;
+            tail_bpjs = newAntrian;
+        }
     }
+    else if (isBPJS == "Tidak")
+    {
+        newAntrian -> no_antrian = "A" + to_string(no_antrian_general++);
+        if (head_general == NULL)
+        {
+            head_general = newAntrian;
+            tail_general = newAntrian;
+        }
+        else
+        {
+            tail_general -> next = newAntrian;
+            newAntrian->prev = tail_general;
+            tail_general = newAntrian;
+        }
+    }
+    cout << "No Antrian: " << newAntrian -> no_antrian << endl;
 }
 
 
