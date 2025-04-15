96e49414bf599cf757c6f09d88098917db54e160
Harmoni Natanael S <harmoninatanaels@gmail.com>
2025-04-15 14:00:49 +0700

    Harmoni | Ongoing

--- src/design/LihatHistory.cpp
+++ src/design/LihatHistory.cpp
@@ -3,9 +3,7 @@
 #include "history.hpp"
 
 void d_LihatHistory(history *temp){
-
     if(temp != NULL) temp = tail_history;
-    cout << temp -> no_antrian;
 
     cout << "-----------------------------------------------------------------" << endl;
     cout << "   No Antrian      : " << temp -> no_antrian << endl;;
@@ -13,24 +11,23 @@ void d_LihatHistory(history *temp){
     cout << "   Keluhan Pasien  : " << temp -> keluhan << endl;;
     cout << "   Resep Obat: " << endl;
     for(int i = 0; i < 10; i++){
-        if(temp -> resep_obat[i]){
-            cout << "   " << i << ". " << temp->resep_obat[i][x]
-        }
+        if(temp -> resep_obat[i]) cout << "   " << i << ". " << temp->resep_obat[i][1] << "(" << temp->resep_obat[i][2] << ")         Rp. " << temp -> resep_obat[i][3] << endl;
     }
-    cout << "-----------------------------------------------------------------";
-    cout << "|  1 - Lanjut List                                               |";
-    cout << "|  2 - Mundur List                                               |";
-    cout << "-----------------------------------------------------------------";
+    cout << "   Antrian " << (temp->is_bpjs ? "BPJS" : "Prioritas") << endl << endl;
+    cout << "-----------------------------------------------------------------" << endl;
+    cout << "|  1 - Lanjut List                                               |" << endl;
+    cout << "|  2 - Mundur List                                               |" << endl;
+    cout << "-----------------------------------------------------------------" << endl;
     while(true){
-        cout << "\nSelect a feature? "; int confirmation;cin >> confirmation;
+        cout << "\nPilih Fitur? "; int confirmation;cin >> confirmation;
         switch (confirmation) {
             case 1:
-                if(temp->prev == NULL) break;
-                return d_LihatHistory(temp->next);
+                // if(temp->prev == NULL) break;
+                // return d_LihatHistory(temp->next);
             break;
             default:
-                if(temp->prev == NULL) break;
-                return d_LihatHistory(temp->prev);
+                // if(temp->prev == NULL) break;
+                // return d_LihatHistory(temp->prev);
             break;
         }
     }
