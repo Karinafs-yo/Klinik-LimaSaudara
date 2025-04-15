a57a556d575a177822b95af718b9610af0dfc777
aylaputri <aylaputri12@upi.edu>
2025-04-15 16:16:04 +0700

    Ayla | Fix Lihat Obat

--- src/design/LihatObat.cpp
+++ src/design/LihatObat.cpp
@@ -1,9 +1,25 @@
 #include "menu.hpp"
 #include "constant.hpp"
+#include "iomanip"
 
 void d_LihatObat(){
-    for (int i = 0; i < sizeof(obat) / sizeof(string); i++)
+    cout << left << setw(5) << "No" 
+         << setw(25) << "Nama Obat" 
+         << setw(10) << "Harga" 
+         << setw(10) << "Stok" << endl;
+
+    cout << string(50, '-') << endl;
+
+    for (int i = 0; i < 50; i++)
     {
-        cout << obat[i] << "\t" << harga[i] << "\t" << stok[i] << endl;
+        cout << left << setw(5) << (i + 1)
+             << setw(25) << obat[i]
+             << setw(10) << harga[i]
+             << setw(10) << stok[i]
+             << endl;
     }
+
+    cout << "\n\n----------------------------------------------------------------" << endl;
+    cout << "                       Klik 1 untuk Kembali                     " << endl;
+    cout << "-------------------------------------------------------- Ketik: "; int c; cin >> c; c ? d_MenuUtama() : d_MenuUtama(); 
 }
