d0da17799f2e85a993648af875d6e5ca0811be6f
rifdahm12 <rifdahm12@upi.edu>
2025-04-16 07:52:25 +0700

    Rifdah | Benerin error

--- src/design/BuatAntrianBaru.cpp
+++ src/design/BuatAntrianBaru.cpp
@@ -11,19 +11,11 @@ void d_BuatAntrianBaru()
     cout << "-------------------- Buat Antrian Baru --------------------" << endl;
     cout << "Pengguna BPJS (Ya/Tidak)? "; cin >> isBPJS;
     
-<<<<<<< HEAD
-    string no_antrian = createAntrian(isBPJS); //generate nomor antrian
-
-    cout << "No Antrian: " << no_antrian; cout << endl;
-    cout << "-----------------------------------------------------------" << endl;
-    cout << "-------------- Nomor Antrian Berhasil Dibuat! -------------" << endl;
-=======
-    string no_antrian = createAntrian(isBPJS);
+    string no_antrian = createAntrian(isBPJS); // generate nomor antrian
     clearTerminal();
     cout << endl << "-------------- Nomor Antrian Berhasil Dibuat! -------------" << endl;
     cout << "                           " << no_antrian << "                                " << endl;
     cout << "-----------------------------------------------------------" << endl << endl;
->>>>>>> 03233d11b1601e390490eb78324ee92756379014
     cout << "Apakah ingin membuat antrian baru lagi? (Ya/Tidak) = "; cin >> isBPJS;
     if (isBPJS == "Ya" || isBPJS == "ya")
     {
--- src/design/LayaniAntrian.cpp
+++ src/design/LayaniAntrian.cpp
@@ -20,25 +20,12 @@ void d_LayaniAntrian()
     } else cin >> temp->keluhan;
 
     cout << "   Resep     : \n";
-<<<<<<< HEAD
-   
     // Cek apakah array resep_obat kosong atau tidak
-    if((sizeof(temp->resep_obat) / sizeof(temp->resep_obat[0])) < 1){
+    if(temp->banyak_resep < 1){
         d_PilihObat(temp); // Kalau kosong, panggil fungsi untuk memilih obat
     } else {
         for (int i = 0; i < temp->banyak_resep; i++) // Kalau sudah ada, tampilkan semua resep yang telah dipilih
-        cout << "   " << i + 1 << ". " << temp->resep_obat[i][0] 
-             << "(" << temp->resep_obat[i][1] << ")         
-             Rp. " << temp->resep_obat[i][2] << endl;
-    } 
-    cout << "   ";
-    cout << "                            " << (temp->is_bpjs ? "   BPJS  " : "Prioritas") << "                           " << endl;
-    cout << "└───────────────────────────────────────────────────────────────┘";
-=======
-    if(temp->banyak_resep < 1){
-        d_PilihObat(temp);
-    } else {
-        for(int i = 0; i < temp ->banyak_resep; i++){
+        {
             if(temp -> resep_obat[i]){
                 cout << "   " << i + 1 << ". " << temp->resep_obat[i][0] << "(" << temp->resep_obat[i][1] << ")         Rp. " << temp -> resep_obat[i][2] << endl;
             }
@@ -55,5 +42,4 @@ void d_LayaniAntrian()
     cout << "-------------------------------------------------------- Ketik: "; int c; cin >> c; c ? d_MenuUtama() : d_MenuUtama(); 
 
 
->>>>>>> 03233d11b1601e390490eb78324ee92756379014
 }
