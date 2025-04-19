c4b664adeeba589b5ace9c3bbe6ab9222c00f553
Lyapunobu <anaqiahza2006@gmail.com>
2025-04-19 22:14:14 +0700

    Anaqi | Invalid input preventation fixes to LihatHistory.cpp

--- src/design/LihatHistory.cpp
+++ src/design/LihatHistory.cpp
@@ -17,41 +17,46 @@ void d_LihatHistory(history *temp) // Fungsi untuk melihat riwayat layanan, mene
         this_thread::sleep_for(chrono::milliseconds(1000));
         return;
     }
-    // Menampilkan informasi riwayat
-    cout << "-----------------------------------------------------------------" << endl;
-    cout << "   No Antrian      : " << temp->no_antrian << endl;
-    cout << "   Antrian         : " << (temp->is_bpjs ? "BPJS" : "Prioritas") << endl;
-    cout << "   Nama Pasien     : " << temp->nama_pasien << endl;
-    cout << "   Keluhan Pasien  : " << temp->keluhan << endl << endl;
-    cout << "   Resep Obat: " << endl;
+
+    while (true){
+        clearTerminal();
     
-       // Loop untuk menampilkan resep obat yang diberikan
-    for (int i = 0; i < temp->banyak_resep; i++) 
-        cout << "   " << i + 1 << ". " << temp->resep_obat[i][0] 
-             << "(" << temp->resep_obat[i][1] << ")         Rp. " 
-             << temp->resep_obat[i][2] << endl;
-    cout << "-----------------------------------------------------------------" << endl;
-    cout << "|  1 - Maju List                                                 |" << endl;
-    cout << "|  2 - Mundur List                                               |" << endl;
-    cout << "|  3 - Kembali ke Menu Utama                                     |" << endl;
-    cout << "-----------------------------------------------------------------" << endl;
-    while (true)
-    {
-        cout << "Pilih Fitur? "; int confirmation; cin >> confirmation;
-        switch (confirmation)
+        // Menampilkan informasi riwayat
+        cout << "-----------------------------------------------------------------" << endl;
+        cout << "   No Antrian      : " << temp->no_antrian << endl;
+        cout << "   Antrian         : " << (temp->is_bpjs ? "BPJS" : "Prioritas") << endl;
+        cout << "   Nama Pasien     : " << temp->nama_pasien << endl;
+        cout << "   Keluhan Pasien  : " << temp->keluhan << endl << endl;
+        cout << "   Resep Obat: " << endl;
+        
+        // Loop untuk menampilkan resep obat yang diberikan
+        for (int i = 0; i < temp->banyak_resep; i++) 
+            cout << "   " << i + 1 << ". " << temp->resep_obat[i][0] 
+                << "(" << temp->resep_obat[i][1] << ")         Rp. " 
+                << temp->resep_obat[i][2] << endl;
+        cout << "-----------------------------------------------------------------" << endl;
+        cout << "|  1 - Maju List                                                 |" << endl;
+        cout << "|  2 - Mundur List                                               |" << endl;
+        cout << "|  3 - Kembali ke Menu Utama                                     |" << endl;
+        cout << "-----------------------------------------------------------------" << endl;
+        while (true)
         {
-        case 1:
-            if (temp->next != NULL) return d_LihatHistory(temp->next);
-            clearOneLine();
-        break;
-        case 2:
-            if (temp->prev != NULL) return d_LihatHistory(temp->prev);
-            clearOneLine();
-        break;
-        default:
-            clearTerminal();
-            d_MenuUtama();
-        break;
+            cout << "Pilih Fitur? "; int confirmation; cin >> confirmation;
+            switch (confirmation)
+            {
+            case 1:
+                if (temp->next != NULL) return d_LihatHistory(temp->next);
+                clearOneLine();
+                break;
+            case 2:
+                if (temp->prev != NULL) return d_LihatHistory(temp->prev);
+                clearOneLine();
+                break;
+            case 3:
+                return;
+            default:
+                continue;
+            }
         }
     }
 }
