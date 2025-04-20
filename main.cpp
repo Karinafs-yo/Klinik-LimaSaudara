9be83a63922b5ee2e84857e6a9095de2dcd46a69
Lyapunobu <anaqiahza2006@gmail.com>
2025-04-20 23:37:00 +0700

    Anaqi | Updated main.cpp to initialize loading CSV file) before running the program

--- main.cpp
+++ main.cpp
@@ -2,10 +2,17 @@
 #include <antrian.hpp>
 #include "menu.hpp"
 #include "utils.hpp"
+#include <constant.hpp>
 using namespace std;
 
 int main()
 {
     // createDummy();
+    
+    const string filename = "src/var/obat.csv";
+    loadData(filename, obat, harga, stok);
+
     d_MenuUtama();
+
+    saveData(filename, obat, harga, stok);
 }
